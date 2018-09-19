4db5f656cf285b0d873b3867bdce1ef6bbdb97a5
bahman2216 <mohammad.ghazavi@gmail.com>
2018-09-19 22:14:23 +0330

    Public view, custom page taxonomy and shortcode

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+/* loading theme style */
 function unite_child_enqueue_styles()
 {
 
@@ -12,54 +13,117 @@ function unite_child_enqueue_styles()
         wp_get_theme()->get('Version')
     );
 }
-
 add_action('wp_enqueue_scripts', 'unite_child_enqueue_styles');
 
+/*
+ * create post type "films" and its taxonomies
+ * */
 function create_post_films()
 {
     register_post_type('films',
         array(
             'labels' => array(
                 'name' => __('Films'),
+                'add_new' => __( 'New film' ),
+                'add_new_item' => __( 'Add New film' ),
+                'edit_item' => __( 'Edit film' ),
+                'new_item' => __( 'New film' ),
+                'view_item' => __( 'View film' ),
+                'search_items' => __( 'Search films' ),
+                'not_found' =>  __( 'No films Found' ),
+                'not_found_in_trash' => __( 'No films found in Trash' ),
             ),
+            'menu_icon' => 'dashicons-format-video',
             'public' => true,
             'hierarchical' => true,
             'has_archive' => true,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'capability_type' => 'page',
             'supports' => array(
                 'title',
                 'editor',
                 'thumbnail',
+	            'page-attributes'
             ),
         )
     );
     register_taxonomy("genre", array("films"), array(
+        'labels' => array(
+	        'name'              => 'Genre',
+	        'singular_name'     => 'Film Genre',
+	        'search_items'      => 'Search Film Genres',
+	        'all_items'         => 'All Film Genres',
+	        'edit_item'         => 'Edit Film Genres',
+	        'update_item'       => 'Update Film Genre',
+	        'add_new_item'      => 'Add New Film Genre',
+	        'new_item_name'     => 'New Film Genre Name',
+	        'menu_name'         => 'Film Genre',
+        ),
         "hierarchical" => false,
-        "label" => "Genre",
-        "singular_label" => "Genre",
-        "rewrite" => true
+        "rewrite" => true,
+        'sort' => true,
+        'args' => array( 'orderby' => 'term_order' ),
+        'show_admin_column' => true
     ));
     register_taxonomy("country", array("films"), array(
-        "hierarchical" => false,
-        "label" => "Country",
-        "singular_label" => "Country",
-        "rewrite" => true
+	    'labels' => array(
+		    'name'              => 'Country',
+		    'singular_name'     => 'Film Country',
+		    'search_items'      => 'Search Film Countries',
+		    'all_items'         => 'All Film Countries',
+		    'edit_item'         => 'Edit Film Countries',
+		    'update_item'       => 'Update Film Country',
+		    'add_new_item'      => 'Add New Film Country',
+		    'new_item_name'     => 'New Film Country Name',
+		    'menu_name'         => 'Film Country',
+	    ),
+	    "hierarchical" => false,
+        "rewrite" => true,
+        'sort' => true,
+        'args' => array( 'orderby' => 'term_order' ),
+        'show_admin_column' => true
     ));
     register_taxonomy("year", array("films"), array(
-        "hierarchical" => false,
-        "label" => "Year",
-        "singular_label" => "Year",
-        "rewrite" => true
+	    'labels' => array(
+		    'name'              => 'Year',
+		    'singular_name'     => 'Film Year',
+		    'search_items'      => 'Search Film Years',
+		    'all_items'         => 'All Film Years',
+		    'edit_item'         => 'Edit Film Years',
+		    'update_item'       => 'Update Film Year',
+		    'add_new_item'      => 'Add New Film Year',
+		    'new_item_name'     => 'New Film Year Name',
+		    'menu_name'         => 'Film Year',
+	    ),
+	    "hierarchical" => false,
+        "rewrite" => true,
+        'sort' => true,
+        'args' => array( 'orderby' => 'term_order' ),
+        'show_admin_column' => true
     ));
     register_taxonomy("actors", array("films"), array(
-        "hierarchical" => false,
-        "label" => "Actors",
+	    'labels' => array(
+		    'name'              => 'Actors',
+		    'singular_name'     => 'Film Actor',
+		    'search_items'      => 'Search Film Actors',
+		    'all_items'         => 'All Film Actors',
+		    'edit_item'         => 'Edit Film Actors',
+		    'update_item'       => 'Update Film Actors',
+		    'add_new_item'      => 'Add New Film Actors',
+		    'new_item_name'     => 'New Film Actors Name',
+		    'menu_name'         => 'Film Actors',
+	    ),
+	    "hierarchical" => false,
         "singular_label" => "Actors",
-        "rewrite" => true
+        "rewrite" => true,
+        'sort' => true,
+        'args' => array( 'orderby' => 'term_order' ),
+        'show_admin_column' => true
     ));
 }
 
 add_action('init', 'create_post_films');
-
 
 function add_films_meta_boxes()
 {
@@ -115,10 +179,6 @@ function set_custom_edit_films_columns($columns) {
     unset( $columns['author'] );
     $columns['ticket_price'] = __( 'Ticket Price', 'ticket_price' );
     $columns['release_date'] = __( 'Release Date', 'release_date' );
-    $columns['genre'] = __( 'Genre', 'genre' );
-    $columns['country'] = __( 'Country', 'country' );
-    $columns['year'] = __( 'Year', 'year' );
-    $columns['actors'] = __( 'Actors', 'actors' );
 
     return $columns;
 }
@@ -127,39 +187,6 @@ function set_custom_edit_films_columns($columns) {
 add_action( 'manage_films_posts_custom_column' , 'custom_films_column', 10, 2 );
 function custom_films_column( $column, $post_id ) {
     switch ( $column ) {
-
-        case 'genre' :
-            $terms = get_the_term_list( $post_id , 'genre' , '' , ',' , '' );
-            if ( is_string( $terms ) )
-                echo $terms;
-            else
-                _e( 'Unable to get genre(s)', '' );
-            break;
-
-        case 'country' :
-            $terms = get_the_term_list( $post_id , 'country' , '' , ',' , '' );
-            if ( is_string( $terms ) )
-                echo $terms;
-            else
-                _e( 'Unable to get country(s)', '' );
-            break;
-
-        case 'year' :
-            $terms = get_the_term_list( $post_id , 'year' , '' , ',' , '' );
-            if ( is_string( $terms ) )
-                echo $terms;
-            else
-                _e( 'Unable to get year', '' );
-            break;
-
-        case 'actors' :
-            $terms = get_the_term_list( $post_id , 'actors' , '' , ',' , '' );
-            if ( is_string( $terms ) )
-                echo $terms;
-            else
-                _e( 'Unable to get actor(s)', '' );
-            break;
-
         case 'ticket_price' :
             echo get_post_meta( $post_id , 'ticket_price' , true );
             break;
@@ -171,3 +198,46 @@ function custom_films_column( $column, $post_id ) {
 }
 
 
+function latest_films( $atts ){
+
+	$args = array(
+		'numberposts' => 5,
+		'offset' => 0,
+		'category' => 0,
+		'orderby' => 'post_date',
+		'order' => 'DESC',
+		'include' => '',
+		'exclude' => '',
+		'meta_key' => '',
+		'meta_value' =>'',
+		'post_type' => 'films',
+		'post_status' => 'publish, future',
+		'suppress_filters' => true
+	);
+
+	$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+
+    $output ='<h2>Recent Films</h2><ul>';
+
+	foreach( $recent_posts as $recent ){
+		$output .='<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+	}
+	wp_reset_query();
+
+	$output .= '</ul>';
+
+	return $output;
+}
+add_shortcode( 'latest_films', 'latest_films' );
+
+/**
+ * @param $form
+ *
+ * @return string
+ * place shortcode under search form
+ */
+function alter_search_form( $form )
+{
+	return $form . do_shortcode('[latest_films]');
+}
+add_filter( 'get_search_form', 'alter_search_form' );
